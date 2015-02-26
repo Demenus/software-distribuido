be0589dec0d0c8cc5289283a605d5b422d752e78
huangchenone <ronhuang@live.cn>
2015-02-26 18:56:26 +0800

    classCard created

--- client/src/model/Card.java
+++ client/src/model/Card.java
@@ -4,10 +4,35 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Huang
  */
 public class Card {
+    public enum Palo {
+        ORO,
+        BASTOS,
+        ESPADAS, 
+        COPAS
+    }
+    private int value;
+    private Palo palo;
+
+    public Card(int value, Palo palo) {
+        this.value = value;
+        this.palo = palo;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public Palo getPalo() {
+        return palo;
+    }
     
+    
+ 
 }
--- client/src/model/Listcard.java
+++ client/src/model/Listcard.java
@@ -4,10 +4,22 @@
  */
 package model;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Huang
  */
 public class Listcard {
+    private ArrayList<Card> cards;
+
+    public Listcard( ) {
+        this.cards = new ArrayList();
+    }
+    public void addCard( Card palo, Card value){
+        //co
+    }
+    
+    
     
 }
