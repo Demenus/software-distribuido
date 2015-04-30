0ddc24c060893449cdb02bf2793e364d83bb0119
Aarón Negrín <negrinaaron@gmail.com>
2015-04-30 18:00:54 +0200

    Changes to web.xml

---
+++ SrcP2/src/LlibreriaServlet.java
@@ -0,0 +1,39 @@
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * Created by aaron on 30/04/2015.
+ */
+public class LlibreriaServlet extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+        super.service(req, res);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("text/html");
+
+        // Actual logic goes here.
+        PrintWriter out = resp.getWriter();
+        out.println("<h1>" + "caca" + "</h1>");
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
